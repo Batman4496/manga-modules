164fed815ea3bb01f8442870760b91f165032e50
Batman4496 <ys861438@gmail.com>
2023-10-02 10:47:32 +0530

    frontend setup

---
+++ frontend/src/App.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <div class="flex flex-col h-screen w-screen text-white bg-blue-950">
+    <header class="bg-blue-900">
+      
+    </header>
+    <main class="h-full">
+      
+    </main>
+    <footer class="bg-blue-900">
+    </footer>
+  </div>
+</template>
